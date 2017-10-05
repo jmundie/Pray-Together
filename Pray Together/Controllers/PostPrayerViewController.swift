9222c00df9b66e7e5219d0c02c0ffd675927e77d
Jason Mundie <jasonmundie@Jasons-MacBook-Pro.local>
2017-10-05 17:09:21 -0500

    bind to Keyboard and clear placeholder text

--- Pray Together/Controllers/PostPrayerViewController.swift
+++ Pray Together/Controllers/PostPrayerViewController.swift
@@ -22,7 +22,8 @@ class PostPrayerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        prayerTextField.delegate = self
         postButton.bindToKeyboard()
         postPrayerLabel.bindToKeyboard()
     }
@@ -36,15 +37,10 @@ class PostPrayerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension PostPrayerViewController: UITextViewDelegate {
+    func textViewDidBeginEditing (_ textView: UITextView) {
+        prayerTextField.text = ""
     }
-    */
-
 }
