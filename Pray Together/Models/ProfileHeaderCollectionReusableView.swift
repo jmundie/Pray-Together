c72e56cc96219057326265f8c5c59ba9a6931c84
Jason Mundie <jasonmundie@Jasons-MacBook-Pro.local>
2017-10-09 17:11:49 -0500

    Profile Header UI

---
+++ Pray Together/Models/ProfileHeaderCollectionReusableView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileHeaderCollectionReusableView.swift
+//  Pray Together
+//
+//  Created by Jason Mundie on 10/9/17.
+//  Copyright © 2017 Jason Mundie. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
