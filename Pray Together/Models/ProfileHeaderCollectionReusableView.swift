97dbd9fa06ab4d91fcd5271b0d67e69beb785ce2
Jason Mundie <jasonmundie@Jasons-MacBook-Pro.local>
2017-10-09 17:22:14 -0500

    profile Header outlets connected

--- Pray Together/Models/ProfileHeaderCollectionReusableView.swift
+++ Pray Together/Models/ProfileHeaderCollectionReusableView.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ProfileHeaderCollectionReusableView: UICollectionReusableView {
 
+    //OUTLETS
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var prayersLabel: UILabel!
+    @IBOutlet weak var organizationsLabel: UILabel!
+    @IBOutlet weak var groupsLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    @IBOutlet weak var followersLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        profileImage.layer.cornerRadius = frame.width/2
+        profileImage.layer.masksToBounds = true
+        profileImage.layer.borderColor = #colorLiteral(red: 0.5352031589, green: 0.6165366173, blue: 0.6980209947, alpha: 1)
+        profileImage.layer.borderWidth = 3
+       
     }
     
+    @IBAction func editProfileButtonPressed(_ sender: Any) {
+    }
 }
